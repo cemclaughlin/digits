7c2604a38291c157a11c58189339ecf997d10468
Cristina McLaughlin <cemclaug@hawaii.edu>
2018-03-16 21:06:12 -0900

    contacts collection finished in 22 minutes

--- app/imports/ui/components/ContactAdmin.jsx
+++ app/imports/ui/components/ContactAdmin.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Card, Button, Image } from 'semantic-ui-react';
+import { Card, Image } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-class Contact extends React.Component {
+class ContactAdmin extends React.Component {
   render() {
     return (
         <Card centered>
@@ -20,15 +20,18 @@ class Contact extends React.Component {
               {this.props.contact.description}
             </Card.Description>
           </Card.Content>
+          <Card.Content extra>
+            {this.props.contact.owner}
+          </Card.Content>
         </Card>
     );
   }
 }
 
 /** Require a document to be passed to this component. */
-Contact.propTypes = {
+ContactAdmin.propTypes = {
   contact: PropTypes.object.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
-export default withRouter(Contact);Contact.jsx
+export default withRouter(ContactAdmin);
